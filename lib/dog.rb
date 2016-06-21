9aed4c854c1b1358b46cb68802e681af304641bf
Jonathan Alexander <jonathandalexander@gmail.com>
2016-06-21 11:29:58 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,86 @@
+#DB[:conn]
+require 'pry'
+class Dog
+  attr_accessor :id, :name, :breed
+
+  #accepts hash/keyword argument with key-value pairs
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.table
+    string = "#{self.to_s.downcase}s"
+    #Dog = dogs
+  end
+
+  def self.create_table
+    sql = "CREATE TABLE IF NOT EXISTS dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT);"
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE #{self.table};"
+    DB[:conn].execute(sql)
+  end
+
+  # row = [id, name, breed]
+  def self.new_from_db(row)
+    new_dog = Dog.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+  def self.find_by_name(name)
+    #look for row in DB with same name
+    sql = "SELECT * FROM dogs WHERE name = ?;"
+    row = DB[:conn].execute(sql, name)[0]
+    #create dog object from db
+    dog = Dog.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
+  def persisted?
+    !!self.id
+  end
+
+  def save
+    sql = "INSERT INTO dogs (name, breed) VALUES (?, ?);"
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    self
+  end
+
+  def self.create(attributes)
+    # did not pass id because the dog isn't saved in the db
+    dog = Dog.new(name: attributes[:name], breed: attributes[:breed])
+    dog.save
+  end
+
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?;"
+    row = DB[:conn].execute(sql, id)[0]
+    dog = Dog.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+  def self.find_or_create_by(attributes)
+    name = attributes[:name]
+    breed = attributes[:breed]
+
+    sql = "SELECT * FROM dogs WHERE name = ? AND breed = ?;"
+    row = DB[:conn].execute(sql, name, breed)[0]
+
+    if row.nil?   
+      # if dog doesn't exist in db create new one (and save)
+      new_dog = self.create(name: name, breed: breed)
+    else
+      #creates new instance of existing dog in DB to return
+      dog = Dog.new(id: row[0], name: row[1], breed: row[2])
+    end
+  end
+
+end
+
